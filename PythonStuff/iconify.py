d8916834e3f12488d38efefd5b77142b21d35d2f
Greg <goldshteyng95@gmail.com>
2017-09-24 13:09:23 -0400

    Added script to turn images into icons, added icons

--- PythonStuff/iconify.py
+++ PythonStuff/iconify.py
@@ -4,18 +4,25 @@ import numpy as np
 def iconify(in_string, out_string):
 	
 	in_image = cv2.imread(in_string)
-	
+	if(in_image == None):
+		return 1
 	height = 34
 	width = 46
 	
 	out_image = range(34)
 	for i in range(34):
-		out_image[i] = range(46)
+		out_image[i] = []
+		for j in range(46):
+			out_image[i] += [[1, 2, 3]]
+	
+	out_image = np.array(out_image)
 	
 	for i in range(37,173,4):
 		for j in range(19,203,4):
-			out_image[i/4][j/4] = in_image[i][j]
+			out_image[(i - 37)/4][(j - 19)/4] = in_image[i][j]
 			
 	cv2.imwrite(out_string, out_image)
 	
 iconify("DoomBladReference.bmp", "DoomIcon.bmp")
+for i in range(204972, 213630):
+	iconify("../m2011pics/PIX_" + str(i) + ".jpg", "../m2011pics/ICON_" + str(i) + ".bmp")
